1f99ca3673477d069845945e199f4f50811ee025
Nicklas Oxhammar <nicklas.oxhammar@hotmail.com>
2018-01-08 18:05:41 +0100

    added pads and soundfiles

--- app/src/main/java/com/example/nick/beatmaker/MainActivity.java
+++ app/src/main/java/com/example/nick/beatmaker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.nick.beatmaker;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void padClicked(View view){
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.nick.beatmaker");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
     }
 }
