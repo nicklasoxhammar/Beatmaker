d831b78aa95cbff832e517a564f90feb95841a6d
Nicklas Oxhammar <nicklas.oxhammar@hotmail.com>
2018-01-10 18:57:53 +0100

    Switched out the soundfiles and changed the colors of the buttons.

--- app/src/main/java/com/example/nick/beatmaker/MainActivity.java
+++ app/src/main/java/com/example/nick/beatmaker/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.nick.beatmaker;
 
-import android.media.MediaPlayer;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SoundPool sp;
+    private int sound1;
+    private int sound2;
+    private int sound3;
+    private int sound4;
+
+
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sp = new SoundPool(50, AudioManager.STREAM_MUSIC,0);
+        sound1 = sp.load(getApplicationContext(),R.raw.hihat,1);
+        sound2 = sp.load(getApplicationContext(),R.raw.crash,1);
+        sound3 = sp.load(getApplicationContext(),R.raw.snare,1);
+        sound4 = sp.load(getApplicationContext(),R.raw.kick,1);
+
+    }
+
+    public void playSoundHihat (View v) {
+        sp.play(sound1,1.0f,1.0f,0,0,1.0f);
+    }
+    public void playSoundCrash (View v) {
+        sp.play(sound2,1.0f,1.0f,0,0,1.0f);
+
+    }
+    public void playSoundSnare (View v) {
+        sp.play(sound3,1.0f,1.0f,0,0,1.0f);
+
+    }
+    public void playSoundKick (View v) {
+        sp.play(sound4,1.0f,1.0f,0,0, 1.0f);
+
+    }
+
+    public void startMetronome(View v){
+
+    }
+
+
+   /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -33,5 +74,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+    }*/
 }
